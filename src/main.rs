7b80ac13d5bed481849b06c3f6ec8cfd61aede0c
Mark Nichols <mark@zanshin.net>
2025-01-12 12:28:56 -0600

    Blockchain array with new, push, pop, and display methods

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,60 @@
 fn main() {
-    println!("Hello Blockchain!");
-    println!(" ");
-    let block = Block::new();
-    println!("value: {}, hash: {}", block.value, block.prev_block_hash);
+    // create a new blockchain
+    let mut blockchain = Blockchain::new();
+
+    // add the initial block to the chain
+    blockchain.push(String::from("Genesis"), 0);
+
+    // add more blocks to the chain
+    blockchain.push(String::from("Transaction 1"), 0);
+    blockchain.push(String::from("Transaction 2"), 0);
+    blockchain.push(String::from("Transaction 3"), 0);
+
+    // display the contents of the chain
+    println!("The blockchain contains:");
+    blockchain.display();
+    
+    if let Some(removed) = blockchain.pop() {
+        println!("Removed element: {}, {}", removed.content, removed.hash);
+    }
+
+    println!("The blockchain contains after removeing last node:");
+    blockchain.display();
+    
 }
 
-// our blockchain is a struct that has value 
-// and a hash of the previous blocl in the chain
+// Block struct - each node is a block
 struct Block {
-    value: String,
-    prev_block_hash: String,
+    content: String,
+    hash: u64,
 }
 
-impl Block {
+// Blockchain - an array of Block structs
+struct Blockchain {
+    items: Vec<Block>,
+}
+
+impl Blockchain {
+
+    // create a new, empty Blockchain
     fn new() -> Self {
-        Self {
-            value: "[Genesis Block]".to_string(),
-            prev_block_hash: "genesis".to_string(),
+        Self { items: Vec::new()  }
+    }
+
+    // add a new block to the chain
+    fn push(&mut self, content: String, hash: u64) {
+        self.items.push(Block { content, hash });
+    }
+
+    // remove a block from the chair
+    fn pop(&mut self) -> Option<Block> {
+        self.items.pop()
+    }
+
+    // display all blocks in the blockchain
+    fn display(&self) {
+        for node in &self.items {
+            println!("Content: {}, Hash: {}", node.content, node.hash);
         }
     }
 }
-
-// make a simple hash of a block. this is very simple (read: insecure) hash
-// function.
