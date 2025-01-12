08e9d04c1c7b9f77ee3e772a97c077b39d032f7c
Mark Nichols <mark@zanshin.net>
2025-01-11 23:15:53 -0600

    Block struct and new impl created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello Blockchain!");
+    println!(" ");
+    let block = Block::new();
+    println!("value: {}, hash: {}", block.value, block.prev_block_hash);
 }
+
+// our blockchain is a struct that has value 
+// and a hash of the previous blocl in the chain
+struct Block {
+    value: String,
+    prev_block_hash: String,
+}
+
+impl Block {
+    fn new() -> Self {
+        Self {
+            value: "[Genesis Block]".to_string(),
+            prev_block_hash: "genesis".to_string(),
+        }
+    }
+}
+
+// make a simple hash of a block. this is very simple (read: insecure) hash
+// function.
