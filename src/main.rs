b0004abb62bd5bce50ea9a424a5b95c46df06095
Mark Nichols <mark@zanshin.net>
2025-01-13 11:11:59 -0600

    Block and Blockchain impls working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+
 fn main() {
     // create a new blockchain
     let mut blockchain = Blockchain::new();
@@ -13,6 +16,13 @@ fn main() {
     // display the contents of the chain
     println!("The blockchain contains:");
     blockchain.display();
+
+    // compute hashes for each block
+    for block in blockchain.iter() {
+        let hash_value = block.compute_hash();
+        println!("Hash value of block: content: {}, hash: {} is {}", 
+            block.content, block.hash, hash_value);
+    }
     
     if let Some(removed) = blockchain.pop() {
         println!("Removed element: {}, {}", removed.content, removed.hash);
@@ -24,12 +34,23 @@ fn main() {
 }
 
 // Block struct - each node is a block
+#[derive(Hash)]
 struct Block {
     content: String,
     hash: u64,
 }
 
+impl Block {
+    // compute a simple hash value for a block
+    fn compute_hash(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        self.hash(&mut hasher);
+        hasher.finish()
+    }
+}
+
 // Blockchain - an array of Block structs
+#[derive(Hash)]
 struct Blockchain {
     items: Vec<Block>,
 }
@@ -46,7 +67,7 @@ impl Blockchain {
         self.items.push(Block { content, hash });
     }
 
-    // remove a block from the chair
+    // remove the last block from the chain
     fn pop(&mut self) -> Option<Block> {
         self.items.pop()
     }
@@ -56,5 +77,10 @@ impl Blockchain {
         for node in &self.items {
             println!("Content: {}, Hash: {}", node.content, node.hash);
         }
+    }
+
+    // create an iterator for the blockchain
+    fn iter(&self) -> std::slice::Iter<'_, Block> {
+        self.items.iter()
     }
 }
